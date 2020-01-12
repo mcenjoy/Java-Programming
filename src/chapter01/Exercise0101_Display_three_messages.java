710dd784f69a9172d0da1bc8eb23e47c526eb423
Michael <mcenjoy@gmail.com>
2020-01-13 01:33:11 +0200

    Initial commit

---
+++ src/chapter01/Exercise0101_Display_three_messages.java
@@ -0,0 +1,14 @@
+/**
+ * (Display three messages) Write a program that displays Welcome to Java,
+ * Learning Java Now, and Programming is fun.
+ *
+ */
+package chapter01;
+
+public class Exercise0101_Display_three_messages {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Java Now");
+        System.out.println("Programming is fun");
+    }
+}
