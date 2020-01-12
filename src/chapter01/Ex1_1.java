dd044c3dfa683e7c8e202586451a9fc186f67527
Michael <mcenjoy@gmail.com>
2020-01-13 01:57:33 +0200

    new

---
+++ src/chapter01/Ex1_1.java
@@ -0,0 +1,13 @@
+/**
+ * (Display three messages) Write a program that displays Welcome to Java,
+ * Learning Java Now, and Programming is fun.
+ */
+package chapter01;
+
+public class Ex1_1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Java Now");
+        System.out.println("Programming is fun");
+    }
+}
---
+++ src/chapter01/Ex1_2.java
@@ -0,0 +1,14 @@
+/**
+ * (Display five messages) Write a program that displays I love Java five times.
+ */
+package chapter01;
+
+public class Ex1_2 {
+    public static void main(String[] args) {
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+        System.out.println("I love Java");
+    }
+}
