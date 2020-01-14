dd7685b7a0850528812e84f5aaabd6c78bea4af2
Michael <mcenjoy@gmail.com>
2020-01-14 08:09:17 +0200

    new

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        double mn = 1.115-0.947;
+        System.out.println(mn*1000/1000.0);
+        System.out.println(Math.round(mn*1000)/1000.0);
     }
 }
