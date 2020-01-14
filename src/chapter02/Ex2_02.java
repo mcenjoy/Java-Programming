30b6c0e0438e4b1082b1b8d8127f77ae1f9200ab
Michael <mcenjoy@gmail.com>
2020-01-14 18:24:15 +0200

    new

--- src/chapter02/Ex2_02.java
+++ src/chapter02/Ex2_02.java
@@ -11,29 +11,34 @@
 
 package chapter02;
 
+import java.math.*;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.*;
 
 public class Ex2_02 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        input.useLocale(Locale.US);
+        DecimalFormatSymbols otherSymbols = new DecimalFormatSymbols(Locale.getDefault());
+        otherSymbols.setDecimalSeparator('.');
+        otherSymbols.setGroupingSeparator(',');
+        DecimalFormat df = new DecimalFormat("####,###.00", otherSymbols);
         System.out.print("Enter length of the sides and height of the Equilateral triangle: ");
         double sideLength = input.nextDouble();
-        double area = rounder((Math.pow(3, 0.5) / 4) * Math.pow(sideLength, 2));
+        double area = round((Math.pow(3, 0.5) / 4) * Math.pow(sideLength, 2), 4);
         double volume = area * sideLength;
 
-        String str = fmt(volume);
-        System.out.println("The area is " + fmt(area));
-        System.out.println("The volume of the Triangular prism is " + str);
+        System.out.println("The area is " + df.format(area));
+        System.out.println("The volume of the Triangular prism is " + df.format(volume));
     }
 
-    public static double rounder(double num) {
-        return Math.round(num * 10000) / 10000.0;
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = BigDecimal.valueOf(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 
-    public static String fmt(double d) {
-        if (d == (long) d)
-            return String.format("%.2f", d);
-        else
-            return String.format("%s", d);
-    }
 }
