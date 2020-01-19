86b88b7809719128c7b6ed190996409fa1460ba8
Michael <mcenjoy@gmail.com>
2020-01-19 22:38:47 +0200

    new

---
+++ src/chapter02/Ex2_13_Financial_application_compound_value.java
@@ -0,0 +1,43 @@
+/*
+ * (Financial application: compound value) Suppose you save $100 each month in a
+ * savings account with annual interest rate 3.75%. Thus, the monthly interest rate is
+ * 0.0375/12 = 0.003125. After the first month, the value in the account becomes
+ * 100 * (1 + 0.003125) = 100.3125
+ * After the second month, the value in the account becomes
+ * (100 + 100.3125) * (1 + 0.003125) = 200.938
+ * After the third month, the value in the account becomes
+ * (100 + 200.938) * (1 + 0.003125) = 301.878
+ * and so on.
+ * Write a program that prompts the user to enter a monthly saving amount and
+ * displays the account value after the sixth month. (In Exercise 5.30, you will use
+ * a loop to simplify the code and display the account value for any month.)
+ *
+ * Enter the monthly saving amount: 100
+ * After the first month, the account value is 100.3125
+ * After the second month, the account value is 200.9384765625
+ * After the third month, the account value is 301.8789093017578
+ * After the sixth month, the account value is 606.5967866995037
+ */
+package chapter02;
+
+import java.util.Scanner;
+
+public class Ex2_13_Financial_application_compound_value {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the monthly saving amount: ");
+        double monthlySavingAmount = input.nextDouble();
+        double accountValue = 0;
+
+        accountValue = (monthlySavingAmount + accountValue) * (1 + 0.003125);
+        System.out.println("After the first month, the account value is " + accountValue);
+        accountValue = (monthlySavingAmount + accountValue) * (1 + 0.003125);
+        System.out.println("After the second month, the account value is " + accountValue);
+        accountValue = (monthlySavingAmount + accountValue) * (1 + 0.003125);
+        System.out.println("After the third month, the account value is " + accountValue);
+        accountValue = (monthlySavingAmount + accountValue) * (1 + 0.003125);
+        accountValue = (monthlySavingAmount + accountValue) * (1 + 0.003125);
+        accountValue = (monthlySavingAmount + accountValue) * (1 + 0.003125);
+        System.out.println("After the sixth month, the account value is " + accountValue);
+    }
+}
